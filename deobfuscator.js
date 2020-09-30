c58a14bef8a80481a6920903b5f977f6c2795c9f
sd-soleaio <sd.soleaio@gmail.com>
2020-09-30 21:06:30 +0100

    initial commit

---
+++ deobfuscator.js
@@ -0,0 +1,116 @@
+const esprima = require('esprima');
+const fs = require('fs');
+const beautify = require('js-beautify').js;
+const utils = require('./utils');
+const { exit } = require('process');
+
+
+let source = fs.readFileSync('input/source.js').toString();
+
+
+// remove any self defending expressions (these will break our string deobfuscation)
+let editedSource = source;
+esprima.parseScript(source, {}, (node, meta) => {
+    if (utils.isSelfDefendingExpression(node) || utils.isUnarySelfDefendingExpression(node)) {
+        let nodeSource = source.substring(meta.start.offset, meta.end.offset);
+        editedSource = editedSource.replace(nodeSource, 'return true;');
+        console.log('Removed a self defending expression');
+    }
+});
+
+
+// find and eval the string array and string shifter functions
+let functionName;
+let shiftSource;
+let startIndex;
+editedSource = source;
+esprima.parseScript(source, { range: true }, (node, meta) => {
+    if (utils.isArrayShifter(node)) {
+        functionName = node.id.name;
+        shiftSource = editedSource.substring(0, meta.end.offset);
+        startIndex = meta.end.offset + 1;
+    }
+});
+
+if (!functionName || !shiftSource || !startIndex) {
+    console.log('Unrecognized obfuscation');
+    exit(1);
+}
+eval(shiftSource);
+editedSource = editedSource.substring(startIndex);
+
+
+// detect and replace string obfuscation function reassignments
+let replaced = true;
+while (replaced) {
+    replaced = false;
+    let source = editedSource;
+    esprima.parseScript(source, {}, (node, meta) => {
+        if (utils.isArrayShifterReassignment(node, functionName)) {
+            let re = new RegExp(node.declarations[0].id.name, 'g');
+            editedSource = editedSource.replace(re, functionName);
+            replaced = true;
+        }
+    });
+}
+
+
+// undo string obfuscation
+source = editedSource;
+esprima.parseScript(source, {}, (node, meta) => {
+    if (utils.isStringUse(node, functionName)) {
+        let res = eval(`${functionName}(${node.arguments[0].value})`);
+        if (res) {
+            let nodeSource = source.substring(meta.start.offset, meta.end.offset);
+            let string = unescape(res).replace(/'/g, "\\'");
+            editedSource = editedSource.replace(nodeSource, `'${string}'`);
+        }
+    }
+});
+
+
+// unescape all strings
+source = editedSource;
+esprima.parseScript(source, {}, (node, meta) => {
+    if (node.type == 'Literal' && typeof node.value == 'string' && node.value != node.raw) {
+        if (node.value != '\n') {
+            let string = eval(node.raw).replace(/'/g, "\\'");
+            let nodeSource = source.substring(meta.start.offset, meta.end.offset);
+            editedSource = editedSource.replace(nodeSource, `'${string}'`);
+        }
+    }
+});
+
+
+// remove any self defending expressions that have now been unveiled
+source = editedSource;
+esprima.parseScript(source, {}, (node, meta) => {
+    if (utils.isSelfDefendingExpression(node) || utils.isUnarySelfDefendingExpression(node)) {
+        let nodeSource = source.substring(meta.start.offset, meta.end.offset);
+        editedSource = editedSource.replace(nodeSource, 'return true;');
+        console.log('Removed a self defending expression');
+    }
+});
+
+
+// make variable names easier on the eyes
+source = editedSource;
+let mappings = {};
+let varIndex = 1;
+esprima.parseScript(source, {}, (node, meta) => {
+    if (node.type == 'Identifier' && node.name.startsWith('_0x')) {
+        while (!mappings[node.name] || utils.keywords.includes(mappings[node.name]))
+            mappings[node.name] = utils.getVariableName(varIndex++);
+
+        let nodeSource = source.substring(meta.start.offset, meta.end.offset);
+        editedSource = editedSource.replace(nodeSource, mappings[node.name]);
+    }
+});
+
+
+// clean up the look of the code, e.g. replace window['chrome'] with window.chrome
+editedSource = editedSource.replace(/(\[("|')([\w\d_$]+)("|')\])/g, '.$3');
+editedSource = editedSource.replace(/\[\.([\w\d_$]+)\]/gi, '\[\[\'$1\'\]\]') // reverses any accidental damage we did with the line above
+
+
+fs.writeFileSync('output/deobfuscated.js', beautify(editedSource));
---
+++ utils.js
@@ -0,0 +1,55 @@
+module.exports = {
+    
+    isArrayShifter: function(node) {
+        return (node.type == 'VariableDeclarator')
+        && (node.id && node.id.type == 'Identifier')
+        && (node.init && node.init.type == 'FunctionExpression')
+        && (node.init.params && node.init.params.length == 2)
+        && (node.init.body && node.init.body.type == 'BlockStatement')
+        && (node.init.body.body[0] && node.init.body.body[0].type == 'ExpressionStatement')
+        && (node.init.body.body[0].expression && node.init.body.body[0].expression.type == 'AssignmentExpression')
+        && (node.init.body.body[0].expression.right && node.init.body.body[0].expression.right.type == 'BinaryExpression');
+    },
+
+    isArrayShifterReassignment: function(node, functionName) {
+        return (node.type == 'VariableDeclaration' && node.declarations)
+        && (node.declarations[0].init && node.declarations[0].init.type == 'Identifier')
+        && (node.declarations[0].id.name != functionName && node.declarations[0].init.name == functionName);
+    },
+
+    isStringUse: function(node, functionName) {
+        return (node.type == 'CallExpression')
+        && (node.callee && node.callee.type == 'Identifier')
+        && (node.callee.name == functionName)
+        && (node.arguments);
+    },
+
+    isSelfDefendingExpression(node) {
+        return (node.type == 'ReturnStatement')
+        && (node.argument && node.argument.type == 'CallExpression')
+        && (node.argument.callee && node.argument.callee.type == 'MemberExpression')
+        && (node.argument.callee.property && node.argument.callee.property.type == 'Literal' && node.argument.callee.property.value == 'test')
+        && (node.argument.arguments && node.argument.arguments.length == 1);
+    },
+
+    isUnarySelfDefendingExpression: function(node) {
+        return (node.type == 'ReturnStatement')
+        && (node.argument && node.argument.type == 'UnaryExpression')
+        && (node.argument.argument && node.argument.argument.type == 'CallExpression')
+        && (node.argument.argument.callee && node.argument.argument.callee.type == 'MemberExpression')
+        && (node.argument.argument.callee.property && node.argument.argument.callee.property.type == 'Literal' && node.argument.argument.callee.property.value == 'test')
+        && (node.argument.argument.arguments && node.argument.argument.arguments.length == 1);
+    },
+
+    getVariableName: function(index) {
+        let name = '';
+        while (index > 0) {
+            t = (index - 1) % 26;
+            name = String.fromCharCode(97 + t) + name;
+            index = (index - t)/26 | 0;
+        }
+        return name;
+    },
+
+    keywords: ['await','break','case','catch','class','const','continue','debugger','default','delete','do','else','enum','export','extends','false','finally','for','function','if','implements','import','in','instanceof','interface','let','new','null','package','private','protected','public','return','super','switch','static','this','throw','try','True','typeof','var','void','while','with','yield']
+}
